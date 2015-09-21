bbb22e787fadd6fea1664cfc976778a7b54725fe
Rémi Vion <vion.remi@gmail.com>
2015-09-21 09:07:46 +0200

    initial commit

--- src/Main.hs
+++ src/Main.hs
@@ -1,14 +1,25 @@
 {-# LANGUAGE OverloadedStrings #-}
 module Main where
 
-import Web.Spock
-import Control.Monad
+import Web.Spock (runSpock,spockT,middleware, file, get, var, text)
+import Control.Monad ()
 import qualified Data.Text as T
 import Data.Text (Text)
+import Network.Wai.Middleware.Static (staticPolicy, addBase)
+import Data.Aeson ()
+import System.Process ()
 
 main :: IO ()
 main =
   runSpock 3000 $ spockT id $ do
+    middleware $ staticPolicy (addBase "static/")
     get "" $ file "test" "static/index.html"
+    -- post "save" $ json ok
     get ("echo" <//> var) $ \something ->
       text $ T.concat ["Echo: ", something]
+
+
+
+
+
+installGitWebUI = "curl https://raw.githubusercontent.com/alberthier/git-webui/master/install/installer.sh | bash"
