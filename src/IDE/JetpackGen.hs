2fcbdf09d626983bc5e08a229546032d38bc1b3c
Rémi Vion <vion.remi@gmail.com>
2015-12-10 18:49:15 +0100

    yay, cabal gen ~working

--- src/IDE/JetpackGen.hs
+++ src/IDE/JetpackGen.hs
@@ -5,19 +5,23 @@ import IDE.State
 import Data.List (isPrefixOf)
 import Data.Monoid
 import Control.Monad
-
+import Data.List (intersperse)
+import Data.Char
 import Data.Map (Map)
+import System.Directory (createDirectoryIfMissing)
 import qualified Data.Map as M
 
 jetpackGen :: IO ()
 jetpackGen = do
-  dbs       <- load "package DB" getDB
-  packages  <- load "packages" (concat <$> mapM getPackageInfos dbs)
-  modules   <- load "modules"  (return $ M.fromList $ concatMap packageModulesIface packages)
-  reexports <- load "reexports plan" (parseReexports)
+  dbs <- load "package DB" getDB
+  packages <- load "packages" (concat <$> mapM getPackageInfos dbs)
+  modules <- load "modules"  (return $ M.fromList $ concatMap packageModulesIface packages)
+  reexports <- load "reexports plan" parseReexports
+  deps <- load "cabal deps" parseDeps
   forM_ reexports $ \(prefix, mod) -> do
     print ("reexport " <> mod)
     printReexports (prefix, modules M.! mod)
+  writeCabalFile reexports deps
   print "done"
   reexportedPackages <- return ()
   return ()
@@ -25,8 +29,12 @@ jetpackGen = do
 parseReexports :: IO [(String, String)]
 parseReexports = do
   f <- readFile "imports.md"
-  let reexports = map ((\(x:y:_) -> (x,y)). words . drop 4) $ filter (isPrefixOf "  - ") $ lines f
-  return reexports
+  return $ map ((\(x:y:_) -> (x,y)). words . drop 4) $ filter (isPrefixOf "  - ") $ lines f
+
+parseDeps :: IO [String]
+parseDeps = do
+  f <- readFile "imports.md"
+  return $ map (drop 4) $ filter (isPrefixOf "  * ") $ lines f
 
 load :: (Traversable a) => String -> IO (a b) -> IO (a b)
 load str action = do
@@ -36,3 +44,40 @@ load str action = do
   return result
 
 for = flip map
+
+writeCabalFile :: [(String, String)] -> [String] -> IO ()
+writeCabalFile reexports deps = writeFile "jetpack/jetpack.cabal" content
+  where
+    content = concat $
+      [ "\nname:                jetpack"
+      , "\nversion:             0.1.0.0"
+      , "\nsynopsis:            Initial project template from stack"
+      , "\ndescription:         Please see README.md"
+      , "\nhomepage:            http://github.com/githubuser/jetpack#readme"
+      , "\nlicense:             BSD3"
+      , "\nauthor:              Rémi Vion"
+      , "\nmaintainer:          vion.remi@gmail.com"
+      , "\ncopyright:           2016 Author Here"
+      , "\ncategory:            Prelude"
+      , "\nbuild-type:          Simple"
+      , "\ncabal-version:       >=1.10"
+      , "\n"
+      , "\nlibrary"
+      , "\n  hs-source-dirs:      src"
+      , "\n  exposed-modules:     JetPack"
+      , "\n  other-modules:       "] ++ intersperse ", " (map toN reexports) ++
+      [ "\n  build-depends:       "] ++ intersperse ", " deps ++
+      [ "\n  default-language:    Haskell2010"
+      , "\n"
+      , "\nsource-repository head"
+      , "\n  type:     git"
+      , "\n  location: https://github.com/rvion/jetpack"
+      ]
+
+toN (prefix, mod) = concat [mod, ".", "As", _typePrefix]
+  where
+    (p:ps) = prefix
+    _idPrefix = (toLower p : ps)
+    _typePrefix = (toUpper p : ps)
+
+
