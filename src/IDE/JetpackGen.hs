7b754d9cde239fc0e14d1c3ae494c97e106e2511
Rémi Vion <vion.remi@gmail.com>
2015-12-12 15:19:12 +0100

    move gcc extensions to cabal file

--- src/IDE/JetpackGen.hs
+++ src/IDE/JetpackGen.hs
@@ -81,8 +81,8 @@ writeCabalFile reexports deps = writeFile "jetpack/jetpack.cabal" content
       , "\n  other-modules:       "]
       ++ intersperse ", " (map toN reexports) ++
       [ "\n  build-depends:       "] ++ intersperse ", " deps ++
-      [ "\n  ghc-options:         -threaded -rtsopts -with-rtsopts=-N -Wall"
       [ "\n  ghc-options:         -threaded -rtsopts -with-rtsopts=-N -Wall -fno-warn-missing-signatures"
+      , "\n  default-extensions:  NoMonomorphismRestriction, FlexibleContexts, MagicHash"
       , "\n  default-language:    Haskell2010"
       , "\n"
       , "\nsource-repository head"
